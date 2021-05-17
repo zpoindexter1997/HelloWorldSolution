90776dec369cd2085f2937978a560d32b7f5ba31
ZhaQuon Poindexter <zpoindexter@tql.com>
2021-05-17 16:37:36 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I PITY THE FOOL!");
+            Console.WriteLine("DON'T TOUCH MY KEYBOARD");
+        }
+    }
+}
